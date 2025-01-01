d92d94464b805ccc314159864cb0b2a51ea1a71f
ishathameemaa <ishathameemaa@gmail.com>
2025-01-01 22:31:47 +0530

    second commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors({
-    origin: 'http://localhost:5174',
+    origin: 'https://lebaba.tomatoes.shop',
     credentials: true
 })
 )
